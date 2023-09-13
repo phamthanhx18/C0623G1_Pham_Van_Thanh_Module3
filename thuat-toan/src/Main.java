d594a3ac53dcaa1561176afc1c1a43456bfacc2d
phamthanhx18 <phamthanhx18@gmail.com>
2023-09-13 18:10:21 +0700

    update task cs

--- thuat-toan/src/Main.java
+++ thuat-toan/src/Main.java
@@ -3,28 +3,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         String s = "abcbdcef";
-        System.out.println(Arrays.toString(solution(s)));
+        System.out.println(solution(s));
     }
     public static char[] solution(String s) {
-        String[] ars = s.split("");
-        List<String> c = new ArrayList<>();
         List<Character> r = new ArrayList<>();
-        for (String ss: ars) {
-            if (!c.contains(ss)){
-                c.add(ss);
-            } else {
-                r.add(ss.charAt(0));
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (s.indexOf(ch) != s.lastIndexOf(ch) && !r.contains(ch)) {
+                r.add(ch);
             }
         }
-        if (r.size()>=1) {
-            char[] ch = new char[r.size()];
-            for (int i = 0; i < ch.length; i++) {
-                ch[i] = r.get(i);
-            }
-            return ch;
-        } else {
-            return new char[0];
+        char[] c = new char[r.size()];
+        for (int i = 0; i < r.size(); i++) {
+            c[i] = r.get(i);
         }
+        return c;
     }
-
 }
